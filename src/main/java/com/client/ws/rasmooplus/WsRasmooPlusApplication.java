5a8001f67e18218b7f931f32bc0257c1917acf62
Felipe Alves <felipe.alves@email.com>
2022-04-07 02:26:02 -0300

    Criacao do projeto

---
+++ src/main/java/com/client/ws/rasmooplus/WsRasmooPlusApplication.java
@@ -0,0 +1,13 @@
+package com.client.ws.rasmooplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsRasmooPlusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsRasmooPlusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/client/ws/rasmooplus/WsRasmooPlusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.client.ws.rasmooplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsRasmooPlusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
