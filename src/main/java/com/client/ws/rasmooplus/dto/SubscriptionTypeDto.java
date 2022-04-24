0141419ae70aa270737be0a6e88e62568da3a7ea
Felipe Alves <felipe.alves@email.com>
2022-04-24 18:48:56 -0300

    Javax validation

--- src/main/java/com/client/ws/rasmooplus/dto/SubscriptionTypeDto.java
+++ src/main/java/com/client/ws/rasmooplus/dto/SubscriptionTypeDto.java
@@ -5,7 +5,10 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.math.BigDecimal;
 
 @Data
@@ -16,11 +19,17 @@ public class SubscriptionTypeDto {
 
     private Long id;
 
+    @NotBlank(message = "campo name não pode ser nulo ou vazio")
+    @Size(min = 5, max = 30)
     private String name;
 
-    private Long accessMonth;
+    @Max(value = 12, message = "campo accessMonths não pode ser maior que 12")
+    private Long accessMonths;
 
+    @NotNull(message = "campo price não pode ser nulo")
     private BigDecimal price;
 
+    @NotBlank(message = "campo productKey não pode ser nulo ou vazio")
+    @Size(min = 5, max = 15, message = "campo productKey deve ter tamanho entre 5 e 15")
     private String productKey;
 }
