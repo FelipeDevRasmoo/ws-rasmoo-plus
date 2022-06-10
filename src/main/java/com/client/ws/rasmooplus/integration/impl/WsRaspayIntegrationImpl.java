a4dbf9895cbb1a42499bbf78858459dcceae99f7
Felipe Alves <felipe.alves@email.com>
2022-06-09 23:54:07 -0300

    inclur autenticacao

--- src/main/java/com/client/ws/rasmooplus/integration/impl/WsRaspayIntegrationImpl.java
+++ src/main/java/com/client/ws/rasmooplus/integration/impl/WsRaspayIntegrationImpl.java
@@ -4,7 +4,9 @@ import com.client.ws.rasmooplus.dto.wsraspay.CustomerDto;
 import com.client.ws.rasmooplus.dto.wsraspay.OrderDto;
 import com.client.ws.rasmooplus.dto.wsraspay.PaymentDto;
 import com.client.ws.rasmooplus.integration.WsRaspayIntegration;
+import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
@@ -22,7 +24,8 @@ public class WsRaspayIntegrationImpl implements WsRaspayIntegration {
     @Override
     public CustomerDto createCustomer(CustomerDto dto) {
         try {
-            HttpEntity<CustomerDto> request = new HttpEntity<>(dto);
+            HttpHeaders headers = getHttpHeaders();
+            HttpEntity<CustomerDto> request = new HttpEntity<>(dto,headers);
             ResponseEntity<CustomerDto> response =
                     restTemplate.exchange("http://localhost:8081/ws-raspay/v1/customer", HttpMethod.POST, request, CustomerDto.class);
             return response.getBody();
@@ -30,6 +33,8 @@ public class WsRaspayIntegrationImpl implements WsRaspayIntegration {
             throw e;
         }
     }
+
+
 
     @Override
     public OrderDto createOrder(OrderDto dto) {
@@ -39,5 +44,13 @@ public class WsRaspayIntegrationImpl implements WsRaspayIntegration {
     @Override
     public Boolean processPayment(PaymentDto dto) {
         return null;
+    }
+
+    private HttpHeaders getHttpHeaders() {
+        HttpHeaders headers = new HttpHeaders();
+        String credential = "rasmooplus:r@sm00";
+        String base64 = new String (Base64.encodeBase64(credential.getBytes()));
+        headers.add("Authorization","Basic "+base64);
+        return headers;
     }
 }
