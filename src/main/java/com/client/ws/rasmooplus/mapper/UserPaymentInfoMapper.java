a2ac4851bb20a1dd437c73c7bec7c87ec22db282
Felipe Alves <felipe.alves@email.com>
2022-06-25 19:38:24 -0300

    salva informacoes de pagamento

---
+++ src/main/java/com/client/ws/rasmooplus/mapper/UserPaymentInfoMapper.java
@@ -0,0 +1,22 @@
+package com.client.ws.rasmooplus.mapper;
+
+import com.client.ws.rasmooplus.dto.UserPaymentInfoDto;
+import com.client.ws.rasmooplus.model.User;
+import com.client.ws.rasmooplus.model.UserPaymentInfo;
+
+public class UserPaymentInfoMapper {
+
+    public static UserPaymentInfo fromDtoToEntity(UserPaymentInfoDto dto, User user){
+        return UserPaymentInfo.builder()
+                .id(dto.getId())
+                .cardNumber(dto.getCardNumber())
+                .cardExpirationMonth(dto.getCardExpirationMonth())
+                .cardExpirationYear(dto.getCardExpirationYear())
+                .cardSecurityCode(dto.getCardSecurityCode())
+                .price(dto.getPrice())
+                .dtPayment(dto.getDtPayment())
+                .user(user)
+                .build();
+
+    }
+}
--- src/main/java/com/client/ws/rasmooplus/service/impl/PaymentInfoServiceImpl.java
+++ src/main/java/com/client/ws/rasmooplus/service/impl/PaymentInfoServiceImpl.java
@@ -3,10 +3,14 @@ package com.client.ws.rasmooplus.service.impl;
 import com.client.ws.rasmooplus.dto.PaymentProcessDto;
 import com.client.ws.rasmooplus.exception.BusinessException;
 import com.client.ws.rasmooplus.exception.NotFoudException;
+import com.client.ws.rasmooplus.mapper.UserPaymentInfoMapper;
 import com.client.ws.rasmooplus.model.User;
+import com.client.ws.rasmooplus.model.UserPaymentInfo;
+import com.client.ws.rasmooplus.repositoy.UserPaymentInfoRepository;
 import com.client.ws.rasmooplus.repositoy.UserRepository;
 import com.client.ws.rasmooplus.service.PaymentInfoService;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Objects;
 
@@ -14,9 +18,11 @@ import java.util.Objects;
 public class PaymentInfoServiceImpl implements PaymentInfoService {
 
     private final UserRepository userRepository;
+    private final UserPaymentInfoRepository userPaymentInfoRepository;
 
-    PaymentInfoServiceImpl(UserRepository userRepository){
+    PaymentInfoServiceImpl(UserRepository userRepository, UserPaymentInfoRepository userPaymentInfoRepository){
         this.userRepository = userRepository;
+        this.userPaymentInfoRepository = userPaymentInfoRepository;
     }
 
     @Override
@@ -31,10 +37,13 @@ public class PaymentInfoServiceImpl implements PaymentInfoService {
             throw new BusinessException("Pagamento não pode ser processado pois usuário já possui assinatura");
         }
 
-        //salvar informacoes de pagamento
         //cria ou atualiza usuario raspay
         //cria o pedido de pagamento
         //processa o pagamento
+
+        //salvar informacoes de pagamento
+        UserPaymentInfo userPaymentInfo = UserPaymentInfoMapper.fromDtoToEntity(dto.getUserPaymentInfoDto(),user);
+        userPaymentInfoRepository.save(userPaymentInfo);
         //enviar email de criacao de conta
         //retorna o sucesso ou nao do pagamento
 
