4f75951d9e90306e2f85f730e2d4700792f26f24
Felipe Alves <felipe.alves@email.com>
2022-07-15 09:50:16 -0300

    Adiciona spring mail ao projeto e cria camada de integracao

---
+++ src/main/java/com/client/ws/rasmooplus/integration/Mailintegration.java
@@ -0,0 +1,7 @@
+package com.client.ws.rasmooplus.integration;
+
+public interface Mailintegration {
+
+    void send(String mailTo, String message);
+
+}
---
+++ src/main/java/com/client/ws/rasmooplus/integration/impl/MailIntegrationImpl.java
@@ -0,0 +1,21 @@
+package com.client.ws.rasmooplus.integration.impl;
+
+import com.client.ws.rasmooplus.integration.Mailintegration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+
+public class MailIntegrationImpl implements Mailintegration {
+
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    @Override
+    public void send(String mailTo, String message) {
+        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+        simpleMailMessage.setTo(mailTo);
+        simpleMailMessage.setSubject("Acesso Liberado!");
+        simpleMailMessage.setText("Login :"+mailTo+" Senha: alunorasmoo");
+        javaMailSender.send(simpleMailMessage);
+    }
+}
