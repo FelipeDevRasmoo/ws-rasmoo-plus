3b32ae25d11696f641683f40aa39268bec3fc94c
Felipe Alves <felipe.alves@email.com>
2022-11-29 22:51:33 -0300

    Aplica extendWith e Mock

--- src/test/java/com/client/ws/rasmooplus/service/UserTypeServiceTest.java
+++ src/test/java/com/client/ws/rasmooplus/service/UserTypeServiceTest.java
@@ -1,9 +1,15 @@
 package com.client.ws.rasmooplus.service;
 
 import com.client.ws.rasmooplus.model.jpa.UserType;
+import com.client.ws.rasmooplus.repositoy.jpa.UserTypeRepository;
+import com.client.ws.rasmooplus.service.impl.UserTypeServiceImpl;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -11,11 +17,14 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import java.util.ArrayList;
 import java.util.List;
 
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 class UserTypeServiceTest {
 
-    @MockBean
-    private UserTypeService userTypeService;
+    @Mock
+    private UserTypeRepository userTypeRepository;
+
+    @InjectMocks
+    private UserTypeServiceImpl userTypeService;
 
     @Test
     void findAll() {
@@ -26,8 +35,8 @@ class UserTypeServiceTest {
         userTypeList.add(userType1);
         userTypeList.add(userType2);
 
-        Mockito.when(userTypeService.findAll()).thenReturn(userTypeList);
+        Mockito.when(userTypeRepository.findAll()).thenReturn(userTypeList);
         var result = userTypeService.findAll();
-        Assertions.assertThat(result).isNotEmpty();
+        Assertions.assertThat(result).hasSize(2).isNotEmpty();
     }
 }
