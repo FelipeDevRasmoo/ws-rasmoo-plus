71419a6578b464976eadc3963c9739a161348d1a
Felipe Alves <felipe.alves@email.com>
2022-12-10 00:00:21 -0300

    Adiciona mockito.verify

--- src/test/java/com/client/ws/rasmooplus/service/UserServiceTest.java
+++ src/test/java/com/client/ws/rasmooplus/service/UserServiceTest.java
@@ -17,7 +17,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class UserServiceTest {
@@ -51,7 +51,10 @@ class UserServiceTest {
         user.setUserType(userType);
         when(userRepository.save(user)).thenReturn(user);
 
-        Assertions.assertEquals(user,userService.create(dto)); ;
+        Assertions.assertEquals(user,userService.create(dto));
+
+        verify(userTypeRepository, times(1)).findById(1L);
+        verify(userRepository, times(1)).save(user);
     }
 
 
