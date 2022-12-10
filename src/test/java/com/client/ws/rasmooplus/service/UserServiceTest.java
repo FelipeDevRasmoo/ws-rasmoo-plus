2242849aab5a88930f7b0b3fbad31d0e80bdda32
Felipe Alves <felipe.alves@email.com>
2022-12-10 19:45:13 -0300

    Cenarios de exception

--- src/test/java/com/client/ws/rasmooplus/service/UserServiceTest.java
+++ src/test/java/com/client/ws/rasmooplus/service/UserServiceTest.java
@@ -1,12 +1,16 @@
 package com.client.ws.rasmooplus.service;
 
 import com.client.ws.rasmooplus.dto.UserDto;
+import com.client.ws.rasmooplus.exception.BadRequestException;
+import com.client.ws.rasmooplus.exception.NotFoudException;
 import com.client.ws.rasmooplus.model.jpa.User;
 import com.client.ws.rasmooplus.model.jpa.UserType;
 import com.client.ws.rasmooplus.repositoy.jpa.UserRepository;
 import com.client.ws.rasmooplus.repositoy.jpa.UserTypeRepository;
 import com.client.ws.rasmooplus.service.impl.UserServiceImpl;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -31,17 +35,24 @@ class UserServiceTest {
     @InjectMocks
     private UserServiceImpl userService;
 
-    @Test
-    void given_create_when_idIsNotNullAndUserIsFound_then_returnUserCreated(){
+    private UserDto dto;
 
-        UserDto dto = new UserDto();
+    @BeforeAll
+    public void loadUser() {
+        dto = new UserDto();
+        dto.setId(1L);
         dto.setEmail("felipe@email.com");
         dto.setCpf("11111111111");
         dto.setUserTypeId(1L);
+    }
 
+    @Test
+    void given_create_when_idIsNullAndUserTypeIsFound_then_returnUserCreated(){
         UserType userType = new UserType(1L, "Aluno", "Aluno da plataforma");
 
         when(userTypeRepository.findById(1L)).thenReturn(Optional.of(userType));
+
+        dto.setId(null);
 
         User user = new User();
         user.setEmail(dto.getEmail());
@@ -57,5 +68,25 @@ class UserServiceTest {
         verify(userRepository, times(1)).save(user);
     }
 
+    @Test
+    void given_create_when_idIsNotNull_then_throwBadRequestException(){
 
+
+
+        Assertions.assertThrows(BadRequestException.class, () -> userService.create(dto));
+
+        verify(userTypeRepository, times(0)).findById(any());
+        verify(userRepository, times(0)).save(any());
+    }
+
+    @Test
+    void given_create_when_idIsNullAndUserTypeIsNotFound_then_throwNotFoudException(){
+        dto.setId(null);
+        when(userTypeRepository.findById(1L)).thenReturn(Optional.empty());
+
+        Assertions.assertThrows(NotFoudException.class, () -> userService.create(dto));
+
+        verify(userTypeRepository, times(1)).findById(1L);
+        verify(userRepository, times(0)).save(any());
+    }
 }
