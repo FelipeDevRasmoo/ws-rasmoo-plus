cc97be812e37e383434f0c4278bbc7e2f82b87d5
Felipe Alves <felipe.alves@email.com>
2022-12-19 23:32:05 -0300

    implementa teste dos metodos da atividade

--- src/test/java/com/client/ws/rasmooplus/service/UserDetailsServiceTest.java
+++ src/test/java/com/client/ws/rasmooplus/service/UserDetailsServiceTest.java
@@ -2,9 +2,12 @@ package com.client.ws.rasmooplus.service;
 
 import com.client.ws.rasmooplus.exception.BadRequestException;
 import com.client.ws.rasmooplus.exception.NotFoudException;
+import com.client.ws.rasmooplus.integration.MailIntegration;
 import com.client.ws.rasmooplus.model.jpa.UserCredentials;
 import com.client.ws.rasmooplus.model.jpa.UserType;
+import com.client.ws.rasmooplus.model.redis.UserRecoveryCode;
 import com.client.ws.rasmooplus.repositoy.jpa.UserDetailsRepository;
+import com.client.ws.rasmooplus.repositoy.redis.UserRecoveryCodeRepository;
 import com.client.ws.rasmooplus.service.impl.UserDetailsServiceImpl;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -14,10 +17,16 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.test.util.ReflectionTestUtils;
 
+import java.lang.reflect.Field;
+import java.time.LocalDateTime;
 import java.util.Optional;
+import java.util.Random;
+import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -28,50 +37,84 @@ class UserDetailsServiceTest {
 
     private static final String USERNAME_ALUNO = "felipe@email.com";
     private static final String PASSWORD_ALUNO = "senha123";
+
     @Mock
     private UserDetailsRepository userDetailsRepository;
+
+    @Mock
+    private UserRecoveryCodeRepository userRecoveryCodeRepository;
+
+    @Mock
+    private MailIntegration mailIntegration;
 
     @InjectMocks
     private UserDetailsServiceImpl userDetailsService;
 
-    private UserCredentials userCredentials;
-
-    @BeforeEach
-    public void loadUserCredentials() {
-        UserType userType = new UserType(1L, "aluno", "aluno plataforma");
-        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-        userCredentials = new UserCredentials(1L, USERNAME_ALUNO, encoder.encode(PASSWORD_ALUNO), userType);
-    }
-
-    @BeforeEach
-    public void loadUserRecoveryCode() {
-    }
-
     @Test
     void given_loadUserByUsernameAndPass_when_thereIsUsername_then_returnUserCredentials() {
+        UserCredentials userCredentials = getUserCredentials();
         when(userDetailsRepository.findByUsername(USERNAME_ALUNO)).thenReturn(Optional.of(userCredentials));
         assertEquals(userCredentials, userDetailsService.loadUserByUsernameAndPass(USERNAME_ALUNO, PASSWORD_ALUNO));
         verify(userDetailsRepository, times(1)).findByUsername(USERNAME_ALUNO);
     }
 
+
     @Test
     void given_loadUserByUsernameAndPass_when_thereIsNoUsername_then_throwNotFoundException() {
         when(userDetailsRepository.findByUsername(USERNAME_ALUNO)).thenReturn(Optional.empty());
-        assertEquals(
+        assertEquals("Usuário não encontrado",
                 assertThrows(NotFoudException.class,
                         () -> userDetailsService.loadUserByUsernameAndPass(USERNAME_ALUNO, PASSWORD_ALUNO)
-                ).getLocalizedMessage(), "Usuário não encontrado");
+                ).getLocalizedMessage());
         verify(userDetailsRepository, times(1)).findByUsername(USERNAME_ALUNO);
     }
 
     @Test
     void given_loadUserByUsernameAndPass_when_passIsNotCorrect_then_throwBadRequestException() {
+        UserCredentials userCredentials = getUserCredentials();
         when(userDetailsRepository.findByUsername(USERNAME_ALUNO)).thenReturn(Optional.of(userCredentials));
-        assertEquals(
+        assertEquals("Usuário ou senha inválido",
                 assertThrows(BadRequestException.class,
                         () -> userDetailsService.loadUserByUsernameAndPass(USERNAME_ALUNO, "pass")
-                ).getLocalizedMessage(), "Usuário ou senha inválido");
+                ).getLocalizedMessage());
         verify(userDetailsRepository, times(1)).findByUsername(USERNAME_ALUNO);
     }
 
+    @Test
+    void given_sendRecoveryCode_when_userRecoveryCodeIsFound_then_updateUserAndSendEmail() {
+        UserRecoveryCode userRecoveryCode = new UserRecoveryCode(UUID.randomUUID().toString(), USERNAME_ALUNO, "4065", LocalDateTime.now());
+        when(userRecoveryCodeRepository.findByEmail(USERNAME_ALUNO)).thenReturn(Optional.of(userRecoveryCode));
+        userDetailsService.sendRecoveryCode(USERNAME_ALUNO);
+        verify(userRecoveryCodeRepository, times(1)).save(any());
+        verify(mailIntegration, times(1)).send(any(), any(), any());
+    }
+
+    @Test
+    void given_sendRecoveryCode_when_userRecoveryCodeIsNotFound_then_SaveUserAndSendEmail() {
+        when(userRecoveryCodeRepository.findByEmail(USERNAME_ALUNO)).thenReturn(Optional.empty());
+        when(userDetailsRepository.findByUsername(USERNAME_ALUNO)).thenReturn(Optional.of(getUserCredentials()));
+        userDetailsService.sendRecoveryCode(USERNAME_ALUNO);
+        verify(userRecoveryCodeRepository, times(1)).save(any());
+        verify(mailIntegration, times(1)).send(any(), any(), any());
+    }
+
+    @Test
+    void given_sendRecoveryCode_when_userRecoveryCodeIsNotFoundAndUserDetailsIsNotFound_then_throwNotFoundException() {
+        when(userRecoveryCodeRepository.findByEmail(USERNAME_ALUNO)).thenReturn(Optional.empty());
+        when(userDetailsRepository.findByUsername(USERNAME_ALUNO)).thenReturn(Optional.empty());
+        try {
+            userDetailsService.sendRecoveryCode(USERNAME_ALUNO);
+        } catch (Exception e) {
+            assertEquals(NotFoudException.class, e.getClass());
+            assertEquals("Usuário não encontrado", e.getMessage());
+        }
+        verify(userRecoveryCodeRepository, times(0)).save(any());
+        verify(mailIntegration, times(0)).send(any(), any(), any());
+    }
+
+    private UserCredentials getUserCredentials() {
+        UserType userType = new UserType(1L, "aluno", "aluno plataforma");
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        return new UserCredentials(1L, USERNAME_ALUNO, encoder.encode(PASSWORD_ALUNO), userType);
+    }
 }
