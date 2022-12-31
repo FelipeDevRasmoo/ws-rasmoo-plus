1ed8386f86ccc55207e07c2e37f46b8cd068886b
Felipe Alves <felipe.alves@email.com>
2022-12-30 23:20:27 -0300

    teste findAll subscriptionType

---
+++ src/test/java/com/client/ws/rasmooplus/controller/SubscriptionTypeControllerTest.java
@@ -0,0 +1,41 @@
+package com.client.ws.rasmooplus.controller;
+
+import com.client.ws.rasmooplus.model.jpa.UserType;
+import com.client.ws.rasmooplus.service.SubscriptionTypeService;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@AutoConfigureDataJpa
+@AutoConfigureTestDatabase
+@WebMvcTest(SubscriptionTypeController.class)
+@AutoConfigureMockMvc(addFilters = false)
+@ActiveProfiles(profiles = "test")
+class SubscriptionTypeControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private SubscriptionTypeService subscriptionTypeService;
+
+    @Test
+    void given_findAll_then_returnAllUserType() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/subscription-type"))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+}
