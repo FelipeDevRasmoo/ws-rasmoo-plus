aa4372c078f7115059f93c16ea5a258955b667f2
Felipe Alves <felipe.alves@email.com>
2022-12-30 22:53:46 -0300

    Melhora teste controller

--- src/test/java/com/client/ws/rasmooplus/controller/UserTypeControllerTest.java
+++ src/test/java/com/client/ws/rasmooplus/controller/UserTypeControllerTest.java
@@ -6,10 +6,14 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -20,8 +24,11 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureDataJpa
+@AutoConfigureTestDatabase
+@WebMvcTest(UserTypeController.class)
 @AutoConfigureMockMvc(addFilters = false)
+@ActiveProfiles(profiles = "test")
 class UserTypeControllerTest {
 
     @Autowired
