c5168bc4306c1521fc42f8c447a9e0ebd976009b
Felipe Alves <felipe.alves@email.com>
2022-12-31 00:24:14 -0300

    findById

--- src/main/java/com/client/ws/rasmooplus/controller/SubscriptionTypeController.java
+++ src/main/java/com/client/ws/rasmooplus/controller/SubscriptionTypeController.java
@@ -5,6 +5,7 @@ import com.client.ws.rasmooplus.model.jpa.SubscriptionType;
 import com.client.ws.rasmooplus.service.SubscriptionTypeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -24,7 +25,7 @@ public class SubscriptionTypeController {
         return ResponseEntity.status(HttpStatus.OK).body(subscriptionTypeService.findAll());
     }
 
-    @GetMapping("/{id}")
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<SubscriptionType> findById(@PathVariable("id") Long id) {
         return ResponseEntity.status(HttpStatus.OK).body(subscriptionTypeService.findById(id));
     }
--- src/test/java/com/client/ws/rasmooplus/controller/SubscriptionTypeControllerTest.java
+++ src/test/java/com/client/ws/rasmooplus/controller/SubscriptionTypeControllerTest.java
@@ -1,5 +1,6 @@
 package com.client.ws.rasmooplus.controller;
 
+import com.client.ws.rasmooplus.model.jpa.SubscriptionType;
 import com.client.ws.rasmooplus.model.jpa.UserType;
 import com.client.ws.rasmooplus.service.SubscriptionTypeService;
 import org.junit.jupiter.api.Test;
@@ -10,15 +11,20 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @AutoConfigureDataJpa
 @AutoConfigureTestDatabase
@@ -34,8 +40,22 @@ class SubscriptionTypeControllerTest {
     private SubscriptionTypeService subscriptionTypeService;
 
     @Test
-    void given_findAll_then_returnAllUserType() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/subscription-type"))
-                .andExpect(MockMvcResultMatchers.status().isOk());
+    void given_findAll_then_returnAllSubscriptionTYpe() throws Exception {
+        mockMvc.perform(get("/subscription-type"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void given_findById_whenGetId2_then_returnOneSubscriptionType() throws Exception {
+        SubscriptionType subscriptionType = new SubscriptionType(2L, "VITALICIO", null,
+                BigDecimal.valueOf(997), "FOREVER2022");
+        Mockito.when(subscriptionTypeService.findById(2L)).thenReturn(subscriptionType);
+
+        mockMvc.perform(get("/subscription-type/2").contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", is(2)))
+                .andExpect(jsonPath("$.name", is("VITALICIO")))
+                ;
     }
 }
